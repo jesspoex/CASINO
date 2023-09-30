ab9c92d45862be46f47ee93becee1d9077e68eb8
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-30 14:59:38 +0700

    Update index.ts

--- src/games/Dice/index.ts
+++ src/games/Dice/index.ts
@@ -7,7 +7,7 @@ export default {
   description: `
     Use the slider to pick a number, then roll below that number to win. Lower numbers will increase your potential payout, while higher ones are safer.
   `,
-  creator: '399KgE5gpzFvBB8arZLxA2bes3n4FY7rTMmzifHohPzx',
+  creator: '89mdwFVhcGT5nYzHNABcaiVaQuENnb39jimQ6qczSrQo',
   image,
   theme_color: 'rgb(255 100 144)',
   app: React.lazy(() => import('./App')),
